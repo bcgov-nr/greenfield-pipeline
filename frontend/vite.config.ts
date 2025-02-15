ccc50a36b326c2ae438db7f43927fdf8ea9f1d70
mishraomp <omprakashmishra3978@gmail.com>
2025-02-15 11:36:42 -0800

    chore: trying

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT),
+    port: parseInt(process.env.PORT || '3001'),
     fs: {
       // Allow serving files from one level up to the project root
       allow: ['..'],
